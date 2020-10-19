4a63616f8a420c8402602cd15711c686c8a56ef4
aiidasuarez <aida.suarezgu@elorrieta-errekamari.com>
2020-10-19 20:56:07 +0200

    modificado

--- Ejercicios/src/main/java/Ejercicios/jomario.java
+++ Ejercicios/src/main/java/Ejercicios/jomario.java
@@ -1,5 +1,14 @@
 package Ejercicios;
 
 public class jomario {
+	
 
+	public static void main(String[] args) {
+		       
+        int num1=10;
+        int num2=5;
+ 
+        System.out.println("El resultado de la multiplicación es "+(num1*num2));
+    
+    }
 }
